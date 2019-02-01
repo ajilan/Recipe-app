5aa24295fba7cac2a352996e2c483732f0b31d89
ajilan <ajilann123@gmail.com>
2019-01-31 18:26:39 -0800

    recipe list are added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Recipe } from '../recipe/recipe.model'
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe [] = [
+    new Recipe ('A Test Recipe' , 'This is simply a test', 'https://pinchofyum.com/wp-content/uploads/Chili-Sesame-Zoodles-Recipe.jpg' )
+  ];
   constructor() { }
 
   ngOnInit() {
